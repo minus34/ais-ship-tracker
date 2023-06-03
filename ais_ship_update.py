92ad4a390c20b8a1de992b5959e98c0f54ac76e3
Hugh Saalmans <hugh.saalmans@gmail.com>
2023-06-03 17:59:43 +1000

    testing

--- ais_ship_update.py
+++ ais_ship_update.py
@@ -4,10 +4,13 @@ import websockets
 import json
 from datetime import datetime, timezone
 
+# CORAL PRINCESS (IMO: 9229659, MMSI 310376000)
+
+
 async def connect_ais_stream():
 
     async with websockets.connect("wss://stream.aisstream.io/v0/stream") as websocket:
-        subscribe_message = {"APIKey": "<YOUR API KEY>", "BoundingBoxes": [[[-180, -90], [180, 90]]]}
+        subscribe_message = {"APIKey": "", "BoundingBoxes": [[[-170, -50], [-150, -33]]]}
 
         subscribe_message_json = json.dumps(subscribe_message)
         await websocket.send(subscribe_message_json)
@@ -19,7 +22,9 @@ async def connect_ais_stream():
             if message_type == "PositionReport":
                 # the message parameter contains a key of the message type which contains the message itself
                 ais_message = message['Message']['PositionReport']
-                print(f"[{datetime.now(timezone.utc)}] ShipId: {ais_message['UserID']} Latitude: {ais_message['Latitude']} Longitude: {ais_message['Longitude']}")
+
+                if ais_message['UserID'] == "310376000" or ais_message['UserID'] == "9229659":
+                    print(f"[{datetime.now(timezone.utc)}] ShipId: {ais_message['UserID']} Latitude: {ais_message['Latitude']} Longitude: {ais_message['Longitude']}")
 
 if __name__ == "__main__":
     asyncio.run(connect_ais_stream())
